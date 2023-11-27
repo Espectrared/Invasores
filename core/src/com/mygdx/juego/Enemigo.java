c551c59a4c2c073df5af76f9c5ce60f8070c34a8
Isaac <aramburo.roberto@uabc.edu.mx>
2023-11-26 18:31:10 -0600

    movimiento del enemigo 0

--- core/src/com/mygdx/juego/Enemigo.java
+++ core/src/com/mygdx/juego/Enemigo.java
@@ -8,26 +8,26 @@ public class Enemigo extends Entity{
 
     private float limiteIzquierdo;
     private float limiteDerecho;
-    private float tiempoTranscurrido = 0; // Contador de tiempo
-    private float intervaloDeTiempo = 3; // Intervalo de tiempo en segundos para que los enemigos bajen
+    private float tiempoTranscurrido = 0;
+    private float intervaloDeTiempo = 3;
 
     public Enemigo(Vector2 position, Texture img, float speed) {
         super(position, img, speed);
-        this.limiteIzquierdo = 0; // El límite izquierdo es el borde izquierdo de la pantalla
-        this.limiteDerecho = Gdx.graphics.getWidth() - sprite.getWidth(); // El límite derecho es el borde derecho de la pantalla menos el ancho del sprite
+        this.limiteIzquierdo = 0;
+        this.limiteDerecho = Gdx.graphics.getWidth() - sprite.getWidth();
     }
 
     @Override
     public void update(float deltaTime) {
         if (position.x < limiteIzquierdo || position.x > limiteDerecho) {
-            speed = -speed; // Cambia la dirección cuando alcanza un límite
+            speed = -speed;
         }
-        position.x += deltaTime * speed;  // Ahora los enemigos se moverán en el eje x
+        position.x += deltaTime * speed;
 
-        tiempoTranscurrido += deltaTime; // Actualiza el contador de tiempo
+        tiempoTranscurrido += deltaTime;
         if (tiempoTranscurrido > intervaloDeTiempo) {
-            position.y -= sprite.getHeight() * 0.20; // Los enemigos bajan un 20% de su altura
-            tiempoTranscurrido = 0; // Reinicia el contador de tiempo
+            position.y -= sprite.getHeight() * 0.20;
+            tiempoTranscurrido = 0;
         }
 
         sprite.setPosition(position.x, position.y);
